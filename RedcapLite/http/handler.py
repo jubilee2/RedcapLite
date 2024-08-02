fbc05fc1a0c497d174bd30842346900c64b7145d
jubilee2 <jubilee2@gmail.com>
2024-08-02 21:50:35 +0000

    add APIException

--- RedcapLite/http/handler.py
+++ RedcapLite/http/handler.py
@@ -1,3 +1,5 @@
+from .error import APIException
+
 def response_error_handler(func):
     def wrapper(obj, data):
         data['returnFormat'] = 'json'
@@ -5,19 +7,19 @@ def response_error_handler(func):
         if response.status_code == 200:
             return response
         elif response.status_code == 400:
-            raise Exception(f"Bad Request: {response.json().get('error', 'No message provided')}")
+            raise APIException(f"Bad Request: {response.json().get('error', 'No message provided')}")
         elif response.status_code == 401:
-            raise Exception("Unauthorized: API token was missing or incorrect.")
+            raise APIException("Unauthorized: API token was missing or incorrect.")
         elif response.status_code == 403:
-            raise Exception("Forbidden: You do not have permissions to use the API.")
+            raise APIException("Forbidden: You do not have permissions to use the API.")
         elif response.status_code == 404:
-            raise Exception("Not Found: The URI requested is invalid or the resource does not exist.")
+            raise APIException("Not Found: The URI requested is invalid or the resource does not exist.")
         elif response.status_code == 406:
-            raise Exception("Not Acceptable: The data being imported was formatted incorrectly.")
+            raise APIException("Not Acceptable: The data being imported was formatted incorrectly.")
         elif response.status_code == 500:
-            raise Exception("Internal Server Error: The server encountered an error processing your request.")
+            raise APIException("Internal Server Error: The server encountered an error processing your request.")
         elif response.status_code == 501:
-            raise Exception("Not Implemented: The requested method is not implemented.")
+            raise APIException("Not Implemented: The requested method is not implemented.")
         else:
             raise Exception("Unkown issue.")
     return wrapper
