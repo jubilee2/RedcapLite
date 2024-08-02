cd7d2caf6b4ae104f1e1283511950bf9cd38a62a
jubilee2 <jubilee2@gmail.com>
2024-08-02 17:53:25 +0000

    add _json_api

--- RedcapLite/api/arm.py
+++ RedcapLite/api/arm.py
@@ -1,8 +1,6 @@
 def get_arm(redcap_lite, data):
     data = {
         'content': 'arm',
-        'format': 'json',
         'returnFormat': 'json'
     }
-    r = redcap_lite._post_request(data)
-    return r.json()
+    return redcap_lite._json_api(data)
--- RedcapLite/redcap.py
+++ RedcapLite/redcap.py
@@ -11,6 +11,16 @@ class RedcapLite:
         response = requests.post(self.url, data=data)
         print('HTTP Status: ' + str(response.status_code))
         return response
+    
+    def _csv_api(self, data):
+        data['format'] = 'csv'
+        response = self._post_request(data)
+        return response.text
+
+    def _json_api(self, data):
+        data['format'] = 'json'
+        response = self._post_request(data)
+        return response.json()
 
     def get_arm(self, **kwargs):
         return get_arm(self, kwargs)
