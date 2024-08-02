17d3f0f93022871e25c7b45aa55884b39efeec35
jubilee2 <jubilee2@gmail.com>
2024-08-02 22:16:19 +0000

    field_names API

--- RedcapLite/api/__init__.py
+++ RedcapLite/api/__init__.py
@@ -2,3 +2,4 @@ from .arm import get_arms, import_arms, delete_arms
 from .dag import get_dags, import_dags, delete_dags
 from .user_dag_mapping import get_user_dag_mappings, import_user_dag_mappings
 from .event import get_events, import_events, delete_events
+from .field_names import get_field_names
