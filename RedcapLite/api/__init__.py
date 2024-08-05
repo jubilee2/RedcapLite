f03388600b0a4c8e219453f4b0e02b7bc40433bd
jubilee2 <jubilee2@gmail.com>
2024-08-05 17:17:10 +0000

    pdf api

--- RedcapLite/api/__init__.py
+++ RedcapLite/api/__init__.py
@@ -6,4 +6,4 @@ from .field_names import get_field_names
 from .file import get_file, import_file, delete_file
 from .file_repository import create_folder_file_repository, list_file_repository, export_file_repository, import_file_repository, delete_file_repository
 from .instrument import get_instruments
-
+from .pdf import export_pdf
---
+++ RedcapLite/api/pdf.py
@@ -0,0 +1,8 @@
+def export_pdf(data):
+    new_data = {
+        'content': 'pdf'
+    }
+    for k in ['record','event','instrument','repeat_instance','allRecords','allRecords','compactDisplay']:
+        if k in data:
+            new_data[k] = data[k]
+    return(new_data)
