a4771c4ea8a49aed10229ded1b809a4c574e980d
Jubilee Tan <jubilee2@gmail.com>
2024-08-12 14:30:33 -0500

    WIP

--- redcaplite/redcap.py
+++ redcaplite/redcap.py
@@ -1,15 +1,7 @@
 from redcaplite import api
 from .http import Client
-from typing import List, Optional, TypedDict, Union, Literal
+from typing import List, Optional, TypedDict, Dict, Any, Union, Literal
 
-class ArmData(TypedDict):
-    arm_num: int
-    name: str
-
-class DagData(TypedDict):
-    data_access_group_name: str
-    unique_group_name: str
-    data_access_group_id: Union[int, Literal['']]
 
 class RedcapClient(Client):
     def __init__(self, url: str, token: str):
@@ -17,142 +9,50 @@ class RedcapClient(Client):
 
     # arms
     def get_arms(self, arms: Optional[List[int]] = []):
-        """
-        Retrieve a list of arms.
-
-        Args:
-            arms (list): Optional list of arm numbers to filter by.
-
-        Returns:
-            list: A list of arm objects.
-        """
         return self.post(api.get_arms({'arms': arms}))
 
-    def import_arms(self, data: List[ArmData]):
-        """
-        Import a list of arms into the system.
-
-        Args:
-            data (List[ArmData]): A list of arm data dictionaries.
-                Each dictionary should contain the following keys:
-                    - arm_num (int): The arm number.
-                    - name (str): The arm name.
-
-        Examples:
-            >>> arms = [
-            ...     {"arm_num": 1, "name": "Drug A"},
-            ...     {"arm_num": 3, "name": "Drug C"}
-            ... ]
-            >>> response = client.import_arms(arms)
-            >>> print(response)
-        """
+    def import_arms(self, data: List[Dict[str, Any]]):
         return self.post(api.import_arms({'data': data}))
 
     def delete_arms(self, arms: List[int]):
-        """
-        Delete a list of arms from the system.
-
-        Args:
-            arms (List[int]): A list of arm numbers to delete.
-
-        Examples:
-            >>> arm_numbers = [1, 3, 5]
-            >>> response = client.delete_arms(arm_numbers)
-            >>> print(response)
-        """
         return self.post(api.delete_arms({'arms': arms}))
 
     # dags
     def get_dags(self):
-        """
-        Retrieve a list of DAGs from the system.
-
-        Returns:
-            list: The response from the API, containing a list of DAGs.
-
-        Examples:
-            >>> response = client.get_dags()
-            >>> print(response)
-        """
         return self.post(api.get_dags({}))
 
-    def import_dags(self, data: List[DagData]):
-        """
-        Import a list of DAGs into the system.
-
-        Args:
-            data (List[DagData]): A list of DAG data dictionaries.
-                Each dictionary should contain the following keys:
-                    - data_access_group_name (str): The data access group name.
-                    - unique_group_name (str): The unique group name.
-                    - data_access_group_id (int): The data access group ID.
-
-        Returns:
-            dict: The response from the API.
-
-        Examples:
-            >>> dags = [
-            ...     {
-            ...         data_access_group_name="my_group",
-            ...         unique_group_name="my_unique_group",
-            ...         data_access_group_id=''
-            ...     },
-            ...     {
-            ...         data_access_group_name="my_other_group",
-            ...         unique_group_name="my_other_unique_group",
-            ...         data_access_group_id=''
-            ...     }
-            ... ]
-            >>> response = client.import_dags(dags)
-            >>> print(response)
-        """
+    def import_dags(self, data: List[Dict[str, Any]]):
         return self.post(api.import_dags({'data':data}))
 
     def delete_dags(self, dags: List[str]):
-        """
-        Delete a list of DAGs from the system.
-
-        Args:
-            dags (List[str]): A list of DAG IDs to delete.
-
-        Returns:
-            dict: The response from the API.
-
-        Examples:
-            >>> dag_ids = ["my_dag", "my_other_dag"]
-            >>> response = client.delete_dags(dag_ids)
-            >>> print(response)
-        """
         return self.post(api.delete_dags({'dags':dags}))
 
     # user_dag_mapping
-    def get_user_dag_mappings(self, **kwargs):
-        return self.post(api.get_user_dag_mappings(kwargs))
+    def get_user_dag_mappings(self):
+        return self.post(api.get_user_dag_mappings({}))
 
-    def import_user_dag_mappings(self, data, **kwargs):
-        kwargs['data'] = data
-        return self.post(api.import_user_dag_mappings(kwargs))
+    def import_user_dag_mappings(self, data: List[Dict[str, Any]]):
+        return self.post(api.import_user_dag_mappings({'data': data}))
 
     # events
-    def get_events(self, **kwargs):
-        return self.post(api.get_events(kwargs))
+    def get_events(self, arms: Optional[List[int]] = []):
+        return self.post(api.get_events({'arms': arms}))
 
-    def import_events(self, data, **kwargs):
-        kwargs['data'] = data
-        return self.post(api.import_events(kwargs))
+    def import_events(self, data: List[Dict[str, Any]]):
+        return self.post(api.import_events({'data': data}))
 
-    def delete_events(self, **kwargs):
-        return self.post(api.delete_events(kwargs))
+    def delete_events(self, events: List[str]):
+        return self.post(api.delete_events({'events': events}))
 
     # field_names
     def get_field_names(self, **kwargs):
         return self.post(api.get_field_names(kwargs))
 
     # file
-    def get_file(self, file_dictionary='', **kwargs):
+    def get_file(self, file_dictionary: str = '', **kwargs):
         return self.file_download_api(api.get_file(kwargs), file_dictionary=file_dictionary)
 
-    def import_file(self, file_path, **kwargs):
+    def import_file(self, file_path: str, **kwargs):
         return self.file_upload_api(file_path, api.import_file(kwargs))
 
     def delete_file(self, **kwargs):
