a009fd1016e31682f5500685a1bbfdfc517c84de
jubilee2 <jubilee2@gmail.com>
2024-10-08 10:42:02 -0500

    fix empty return for csv

--- redcaplite/http/handler.py
+++ redcaplite/http/handler.py
@@ -42,6 +42,8 @@ def csv_handler(func):
         response = func(obj, data)
         if 'returnContent' in data and data['returnContent'] == 'ids':
             return response.json()
+        if response.text == '':
+            return pd.DataFrame()  # Return an empty DataFrame if response is empty
         io_string = io.StringIO(response.text)
         df = pd.read_csv(io_string, **pd_read_csv_kwargs)
         io_string.close()
