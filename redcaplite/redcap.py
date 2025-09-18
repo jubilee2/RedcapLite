422e9aac86b61e1ff955d02e130bd2d427982ab6
Jubilee <jubilee2@gmail.com>
2025-09-18 17:01:32 -0500

    fix metadata export for csv

--- redcaplite/redcap.py
+++ redcaplite/redcap.py
@@ -501,7 +501,15 @@ class RedcapClient(Client):
         fields: List[str] = [],
         forms: List[str] = [],
         format: Literal["json", "csv"] = "csv",
-        pd_read_csv_kwargs: Optional[Dict[str, Any]] = {},
+        pd_read_csv_kwargs: Optional[Dict[str, Any]] = {'dtype': {
+            'section_header': str,
+            'field_label': str,
+            'select_choices_or_calculations': str,
+            'field_note': str,
+            'text_validation_type_or_show_slider_number': str,
+            'required_field': str,
+            'custom_alignment': str,
+        }},
     ):
         """
         Export metadata (data dictionary) from the project.
@@ -515,6 +523,7 @@ class RedcapClient(Client):
         Returns:
             The response from the API containing the metadata.
         """
+        {'dtype': {'participant_study_id': str}}
         return self.post(
             api.get_metadata(
                 {"fields": fields, "forms": forms, "format": format}),
