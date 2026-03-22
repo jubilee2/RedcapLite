9ed7f654936dd7e54f8026ca5f67393cea45e5e6
jubilee2 <jubilee2@gmail.com>
2026-03-22 17:38:57 -0500

    Apply suggestion from @jubilee2

--- redcaplite/config/profiles.py
+++ redcaplite/config/profiles.py
@@ -4,8 +4,6 @@ from __future__ import annotations
 
 import os
 import platform
-import site
-import sys
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Dict, Optional
